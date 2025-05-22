9c7c84fcd3a8b48b4da290eda850e63a09c4c029
haricharan61108 <116500176+haricharan61108@users.noreply.github.com>
2025-05-22 19:14:19 +0530

    Done with the worker and LLM Connection

---
+++ packages/db/prisma/migrations/20250520111513_added_actions/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Action" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "content" TEXT NOT NULL,
+    "projectId" TEXT NOT NULL,
+
+    CONSTRAINT "Action_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Action" ADD CONSTRAINT "Action_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
